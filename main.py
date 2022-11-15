1dfda847a2478cc69c8f8e4f1c8afc61c0dfc7ed
Rebecca Etim <101546979+bectimvisuals@users.noreply.github.com>
2022-11-15 16:13:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#print(type(two_digit_number))
+value1 = int(two_digit_number[0])
+value2 = int(two_digit_number[1])
+print(value1 + value2)
+
+
+
+
+
+
+
